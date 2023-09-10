b3492f25021bdbd25eaa730312515847369aea25
ollwhl <ollwhl991118@gmail.com>
2023-09-11 00:23:12 +0900

    plan url bug fix

--- src/main/java/com/hxd/fsystemback/controller/PlaneController.java
+++ src/main/java/com/hxd/fsystemback/controller/PlaneController.java
@@ -12,23 +12,23 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/plane")
+@RequestMapping("/plan")
 
 public class PlaneController {
     @Resource
     PlaneService planeService;
 
-    @GetMapping("/getPlane")
+    @GetMapping("/getPlan")
     public Result getPlane(Params params){
         PageInfo<Product> list =planeService.getPlane(params);
         return Result.success(list);
     }
-    @PostMapping("/editPlane")//传入name，planeNum,planeDate
+    @PostMapping("/editPlan")//传入name，planeNum,planeDate
     public Result editPlane(@RequestBody Product product) throws CustomException, JsonProcessingException {
         planeService.editPlane(product);
         return Result.success();
     }
-    @PostMapping("/delPlane")//传入name
+    @PostMapping("/delPlan")//传入name
     public Result delPlane(@RequestBody Product product) throws CustomException, JsonProcessingException {
         planeService.delPlane(product);
         return Result.success();
