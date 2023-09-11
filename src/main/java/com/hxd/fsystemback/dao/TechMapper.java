d83883715c807207e7361daacc86b0d500f28bc0
ollwhl <ollwhl991118@gmail.com>
2023-09-12 03:06:36 +0900

    plan url bug fix

--- src/main/java/com/hxd/fsystemback/dao/TechMapper.java
+++ src/main/java/com/hxd/fsystemback/dao/TechMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Mapper
 public interface TechMapper {
 
-    @Select("SELECT t.*, p.name AS productname, p.standard AS productstandard,p.note AS productnote, pt.name AS partsname, pt.standard AS partsstandard,pt.`group` As partsGroup FROM tech AS t LEFT JOIN product p ON t.productid = p.id LEFT JOIN parts pt ON t.partsid = pt.id ORDER BY p.name ASC")
+    @Select("SELECT t.*, p.name AS productname, p.standard AS productstandard,p.prewarn,p.note AS productnote, pt.name AS partsname, pt.standard AS partsstandard,pt.`group` As partsGroup FROM tech AS t LEFT JOIN product p ON t.productid = p.id LEFT JOIN parts pt ON t.partsid = pt.id ORDER BY p.name ASC")
     public List<Tech> getProductTech();
     @Select("SELECT * FROM tech WHERE productid = #{productId} AND partsid = #{partsId}")
     public Tech findTechByProductIdAndPartsId(int productId,int partsId);
